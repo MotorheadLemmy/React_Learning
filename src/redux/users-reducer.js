5babf372d75fa54c1955c343505811971baa3711
Jenya <dorolemmydio16@gmail.com>
2020-11-12 23:45:31 +0200

    54lesson pagination

--- src/redux/users-reducer.js
+++ src/redux/users-reducer.js
@@ -1,11 +1,16 @@
 const FOLLOW='FOLLOW'
 const UNFOLLOW='UNFOLLOW'
 const SET_USERS='SET_USERS'
+const SET_CURRENT_PAGE='SET_CURRENT_PAGE'
+const SET_TOTAL_USERS_COUNT='SET_TOTAL_USERS_COUNT'
 
 let initialState={
     users:[
        
       ],
+      pageSize:5,
+      totalUsersCount:0,
+      currentPage:2
      
   }
 const usersReducer=(state=initialState,action)=>{
@@ -36,7 +41,13 @@ const usersReducer=(state=initialState,action)=>{
             })
           }
           case SET_USERS:{
-            return {...state,users:[...state.users,...action.users]}
+            return {...state,users:[...action.users]}
+          }
+          case SET_CURRENT_PAGE:{
+            return {...state, currentPage:action.currentPage}
+          }
+          case SET_TOTAL_USERS_COUNT:{
+            return{...state,totalUsersCount:action.count}
           }
         default:
             return state    
@@ -59,6 +70,16 @@ export const followAC=(userId)=>{
     
     return{
       type:SET_USERS,users
+    }
+  }
+  export const setCurrentPageAC=(currentPage)=>{
+    return{
+      type:SET_CURRENT_PAGE,currentPage
+    }
+  }
+  export const setUsersTotalCountAC=(totalUsersCount)=>{
+    return {
+      type:SET_TOTAL_USERS_COUNT,count:totalUsersCount
     }
   }
 export default usersReducer
