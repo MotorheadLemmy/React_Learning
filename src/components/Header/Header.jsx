4d9d06d5574789baaa7d9a189778a7ba63765cb2
Jenya <dorolemmydio16@gmail.com>
2020-11-15 20:48:02 +0200

    61less login authme

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import s from './Header.module.css';
-const Header=()=>{
+const Header=(props)=>{
     return(
         <header className={s.header}>
         <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSGBEOcjPPVl-dKhMQLag_OaUnSQP-5JbOPiQ&usqp=CAU' />
+      <div className={s.loginBlock}>
+        {props.isAuth ? props.login
+        : <NavLink to={'/login/'}>Login </NavLink>}
+        
+         </div> 
       </header>
     )
 }
--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -2,13 +2,15 @@ import dialogsReducer from "./dialogs-reducer";
 import sidebarReducer from "./sidebar-reducer";
 import profileReducer from "./profile-reducer";
 import usersReducer from "./users-reducer";
+import authReducer from "./auth-reducer";
 
 const { createStore, combineReducers } = require("redux");
 let reducers=combineReducers({
     profilePage:profileReducer,
     dialogsPage:dialogsReducer,
     sidebar:sidebarReducer,
-    usersPage:usersReducer
+    usersPage:usersReducer,
+    auth:authReducer
 
 })
 let store=createStore(reducers)
